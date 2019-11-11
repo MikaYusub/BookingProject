7e48ed0f31f8c6b574d846b0e60cfc7f2b9d4845
maharramg <quliyevff@gmail.com>
2019-11-11 12:05:57 +0400

    Booking Project.

--- src/Controller/Console.java
+++ src/Controller/Console.java
@@ -8,9 +8,10 @@ public class Console {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int item;
+        boolean q = false;
 
         System.out.println("___MENU___");
-
         System.out.println("1.---Online Board---");
         System.out.println("2.---Show the Flight info---");
         System.out.println("3.---Search and Book a Flight---");
@@ -18,39 +19,42 @@ public class Console {
         System.out.println("5.---My Flights---");
         System.out.println("6.---Exit---");
 
-        System.out.println("Please select a menu item by typing corresponding number from 1-6:");
-        int item = scanner.nextInt();
+        do {
+            System.out.println("Please select a menu item by typing corresponding number from 1-6:\n");
+            item = scanner.nextInt();
+            switch (item) {
+                case 1:
+                    //Online-board
+                    System.out.println("\n---Online Board---");
+                    Flights flights = new Flights(1, 100, "Kiev", "Baku");
+                    System.out.println(flights.toString());
 
-        switch (item) {
-            case 1:
-            //Online-board
-            System.out.println("---Online Board---");
-            Flights flights = new Flights(1, 100, "Kiev", "Baku");
-            System.out.println(flights.toString());
-            break;
-            case 2:
-            //flight
-            System.out.println("---Flight info---");
-            break;
-            case 3:
-            //search and book a flight
-            System.out.println("---Search and Book a Flight---");
-            break;
-            case 4:
-            //cancel the booking
-            System.out.println("---Cancel the Booking---");
-            break;
-            case 5:
-            //my flights
-            System.out.println("---My Flights---");
-            break;
-            case 6:
-            //exit
-            System.out.println("---Exit---");
-            break;
-            default:
-                System.out.println("Wrong input");
-        }
+                    break;
+                case 2:
+                    //flight
+                    System.out.println("\n---Flight info---");
+                    break;
+                case 3:
+                    //search and book a flight
+                    System.out.println("\n---Search and Book a Flight---");
+                    break;
+                case 4:
+                    //cancel the booking
+                    System.out.println("\n---Cancel the Booking---");
+                    break;
+                case 5:
+                    //my flights
+                    System.out.println("\n---My Flights---");
+                    break;
+                case 6:
+                    //exit
+                    System.out.println("\nAdios.");
+                    q = true;
+                    break;
+                default:
+                    System.out.println("Wrong input");
+            }
+        }while (!q);
 
     }
 
