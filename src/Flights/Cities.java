5be77d3391d2069edc925f65a2a2c7cc331b721e
maharramg <quliyevff@gmail.com>
2019-11-11 10:55:52 +0400

    Booking Project.

--- src/Flights/Cities.java
+++ src/Flights/Cities.java
@@ -1,4 +1,13 @@
 package Flights;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Cities {
+
+    private List<String> cities = Arrays.asList("Kiev","Baku");
+
+    public List<String> getCities() {
+        return cities;
+    }
 }
--- src/Flights/Flights.java
+++ src/Flights/Flights.java
@@ -1,4 +1,41 @@
 package Flights;
 
 public class Flights {
+    private int id;
+    private int seats;
+    private String cityFrom;
+    private String cityTo;
+
+    public Flights(int id, int seats, String cityFrom, String cityTo) {
+        this.id = id;
+        this.seats = seats;
+        this.cityFrom = cityFrom;
+        this.cityTo = cityTo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getCityFrom() {
+        return cityFrom;
+    }
+
+    public String getCityTo() {
+        return cityTo;
+    }
+
+    @Override
+    public String toString() {
+        return "Flights{" +
+                "id=" + id +
+                ", seats=" + seats +
+                ", cityFrom='" + cityFrom + '\'' +
+                ", cityTo='" + cityTo + '\'' +
+                '}';
+    }
 }
